481bda03dc6d7d3e70004ca0f3026263a9b74ea2
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-11-20 12:50:36 -0300

    Create routes and first endpoint

--- src/clojure_api/core.clj
+++ src/clojure_api/core.clj
@@ -1,6 +1,17 @@
-(ns clojure-api.core)
+(ns clojure-api.core
+  (:require [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]))
 
-(defn -main
-  "I don't do a whole lot."
-  []
-  (println "Hello, World!"))
+(defn func-hello [request]
+  {:status 200 :body (str "Hello World " (get-in request [:query-params :name] "Everybody"))})
+
+(def routes (route/expand-routes
+             #{["/hello" :get func-hello :route-name :hello-world]}))
+
+(def service-map {::http/routes routes
+                  ::http/port 8081
+                  ::http/type :jetty
+                  ::http/join? false})
+
+(http/start (http/create-server service-map))
+(println "started server http")
