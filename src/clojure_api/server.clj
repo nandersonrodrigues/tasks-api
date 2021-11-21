67a93fc0698748660b81f29e0d48798afe20f68e
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-11-21 17:05:29 -0300

    Add task resource and routes
    - post task: createTask()
    - get tasks: listTasks()

--- src/clojure_api/server.clj
+++ src/clojure_api/server.clj
@@ -3,11 +3,32 @@
             [io.pedestal.http :as http]
             [io.pedestal.test :as test]))
 
+(def store (atom {}))
+
+(defn list-tasks [request]
+  {:status 200 :body @store})
+
+(defn create-task-map [uuid name status]
+  {:uuid uuid :name name :status status})
+
+; {id {task_id task_name task_status}}
+(defn create-task [request]
+  (let
+   [uuid (java.util.UUID/randomUUID)
+    name (get-in request [:query-params :name])
+    status (get-in request [:query-params :status])
+    task (create-task-map uuid name status)]
+    (swap! store assoc uuid task)
+    {:status 200 :body {:mensagem "tarefa registrada com sucesso"
+                        :task task}} ))
+
 (defn func-hello [request]
   {:status 200 :body (str "Hello World " (get-in request [:query-params :name] "Everybody"))})
 
 (def routes (route/expand-routes
-             #{["/hello" :get func-hello :route-name :hello-world]}))
+             #{["/hello" :get func-hello :route-name :hello-world]
+               ["/task" :post create-task :route-name :create-task]
+               ["/task" :get list-tasks :route-name :list-tasks]}))
 
 (def service-map {::http/routes routes
                   ::http/port 8081
